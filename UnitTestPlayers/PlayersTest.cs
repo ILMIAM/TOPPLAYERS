4299f634ac0efcce4fd0de6e117f4530359145ab
ILMIAM <mohamed.almia@gmail.com>
2019-09-18 00:34:50 +0200

    add test Project

---
+++ UnitTestPlayers/PlayersTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TOPPLAYERS.Controllers;
+using System.Collections.Generic;
+using System.Web.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UnitTestPlayers
+{
+    [TestClass]
+    public class PlayersTest
+    {
+        [TestMethod]
+        public void  ShouldReturnAllPlayers()
+        {
+            var controller = new PalyerController();
+            var result = controller.Players() ;
+            Assert.AreEqual(5, result.Count);
+        }
+
+        [TestMethod]
+        public void ShouldReturnCorrectPlayer()
+        {
+            var controller = new PalyerController();
+            IActionResult result = controller.GetById(17);
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(OkResult));
+        }
+
+        [TestMethod]
+        public void ShouldReturnDletePlayer()
+        {
+            var controller = new PalyerController();
+            IActionResult actionResult = controller.Delete(17);
+            Assert.IsNotNull(actionResult);
+        }
+
+        [TestMethod]
+        public void ShouldReturnNotFountDletePlayer()
+        {
+            var controller = new PalyerController();
+            IActionResult actionResult = controller.Delete(170);
+          
+            Assert.IsInstanceOfType(actionResult, typeof(NotFoundResult));
+           
+        }
+    }
+}
---
+++ UnitTestPlayers/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestPlayers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestPlayers")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c988779d-6a56-4d31-8251-3178c8ebb6b6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
